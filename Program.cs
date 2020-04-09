d97ff3aa9f6285b4b35eb375f1989fcaf5c8c512
Bruno Noatto Iserhardt <33487330+brunonoatto@users.noreply.github.com>
2020-04-09 13:14:55 -0300

    Desafio Numéria Bruno Noatto Iserhardt
    Realizada a implementação da interface IElevadorService, na implementação a classe recebe os itens do json no construtor, que é utilizado nos métodos.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace DesafioNumeria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = GetItems();
+            var service = new ElevadorService(items);
+
+            Console.WriteLine("andarMenosUtilizado={0}", string.Join(",", service.andarMenosUtilizado()));
+            Console.WriteLine("elevadorMaisFrequentado={0}", string.Join(",", service.elevadorMaisFrequentado()));
+            Console.WriteLine("elevadorMenosFrequentado={0}", string.Join(",", service.elevadorMenosFrequentado()));
+            Console.WriteLine("percentualDeUsoElevadorA={0}", string.Join(",", service.percentualDeUsoElevadorA()));
+            Console.WriteLine("percentualDeUsoElevadorB={0}", string.Join(",", service.percentualDeUsoElevadorB()));
+            Console.WriteLine("percentualDeUsoElevadorC={0}", string.Join(",", service.percentualDeUsoElevadorC()));
+            Console.WriteLine("percentualDeUsoElevadorD={0}", string.Join(",", service.percentualDeUsoElevadorD()));
+            Console.WriteLine("percentualDeUsoElevadorE={0}", string.Join(",", service.percentualDeUsoElevadorE()));
+            Console.WriteLine("periodoMaiorFluxoElevadorMaisFrequentado={0}", string.Join(",", service.periodoMaiorFluxoElevadorMaisFrequentado()));
+            Console.WriteLine("periodoMaiorUtilizacaoConjuntoElevadores={0}", string.Join(",", service.periodoMaiorUtilizacaoConjuntoElevadores()));
+            Console.WriteLine("periodoMenorFluxoElevadorMenosFrequentado={0}", string.Join(",", service.periodoMenorFluxoElevadorMenosFrequentado()));
+            Console.Read();
+        }
+
+        private static List<InputItem> GetItems()
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            var jsonString = File.ReadAllBytes("input.json");
+            List<InputItem> items = JsonSerializer.Deserialize<List<InputItem>>(jsonString, options);
+
+            return items;
+        }
+    }
+}
